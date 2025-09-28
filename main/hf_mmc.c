0742b44ed9de9eb40ae1b67b79cde7db5a4d4bd7
Mubeen Ahmad Fayyaz <mubeenahfa@gmail.com>
2025-09-28 08:10:06 +0300

    added freertos for task management

--- main/hf_mmc.c
+++ main/hf_mmc.c
@@ -1,23 +1,74 @@
 #include <stdio.h>
+#include <string.h>
 #include "algo.h"
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+
+#define MOS_RED   GPIO_NUM_18
+#define MOS_GREEN GPIO_NUM_19
+
+typedef struct {
+    int arr[20];
+    size_t size;
+    int threshold;
+} algo_input_t;
+
+static QueueHandle_t algo_queue;
+//bunu sadece suanlik boyle yaptim sonra sensordan okuyup ekleyecegim
+void sensor_task(void *pvParameter) {
+    while (1) {
+        algo_input_t input;
+        int temp[20] = {
+            250,265,279,290,298,
+            300,298,290,279,265,
+            250,234,221,210,202,
+            200,202,210,221,234
+        };
+        memcpy(input.arr, temp, sizeof(temp));
+        input.size = 20;
+        input.threshold = 10;
+        xQueueSend(algo_queue, &input, portMAX_DELAY);
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
+
+void algo_task(void *pvParameter) {
+    algorithm_t chosen = BLOCKM;
+    algo_func_t func = get_algorithm(chosen);
+    algo_input_t input;
+    while (1) {
+        if (xQueueReceive(algo_queue, &input, portMAX_DELAY) == pdPASS) {
+            if (func) {
+                bool result = func(input.arr, input.size, input.threshold);
+                printf("Algorithm returned: %s\n", result ? "true" : "false");
+                if (result) {
+                    gpio_set_level(MOS_RED, 1);
+                    gpio_set_level(MOS_GREEN, 0);
+                } else {
+                    gpio_set_level(MOS_RED, 0);
+                    gpio_set_level(MOS_GREEN, 1);
+                }
+            }
+        }
+    }
+}
 
 void app_main(void) {
-    int data[20] = {
-    250, 265, 279, 290, 298,
-    300, 298, 290, 279, 265,
-    250, 234, 221, 210, 202,
-    200, 202, 210, 221, 234
-};
-    size_t size = sizeof(data) / sizeof(data[0]);
-    int threshold = 10;
+    gpio_config_t io_conf = {
+        .pin_bit_mask = (1ULL << MOS_RED) | (1ULL << MOS_GREEN),
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = 0,
+        .pull_down_en = 0,
+        .intr_type = GPIO_INTR_DISABLE,
+    };
+    gpio_config(&io_conf);
 
-    algorithm_t chosen = BLOCKM;   // could also be set via Kconfig / runtime input
-    algo_func_t func = get_algorithm(chosen);
+    algo_queue = xQueueCreate(5, sizeof(algo_input_t));
+    if (!algo_queue) return;
 
-    if (func) {
-        bool result = func(data, size, threshold);
-        printf("Algorithm returned: %s\n", result ? "true" : "false");
-    } else {
-        printf("Invalid algorithm!\n");
-    }
+    xTaskCreate(sensor_task, "sensor_task", 2048, NULL, 5, NULL);
+    xTaskCreate(algo_task, "algo_task", 2048, NULL, 5, NULL);
 }
